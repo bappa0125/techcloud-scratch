8997d43f0c301ddf06d477a1857a36652de9a4a0
bappa0125 <subrata.fouzder@gmail.com>
2019-12-24 10:15:58 +0530

    updated a lot

--- src/views/Dashboard copy.vue
+++ src/views/Dashboard copy.vue
@@ -1,104 +1,124 @@
 <template>
-  <div class="dashboard">
-    <h1 class="subheading grey--text">Dashboard</h1>
+  <div class="team">
+    <h1 class="subheading grey--text">Choose an Account type you want to vend</h1>
 
     <v-container class="my-5">
 
-      <v-layout row justify-start class="mb-3">
-        <v-tooltip top>
-          <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
-            <v-icon small left>folder</v-icon>
-            <span class="caption text-lowercase">By project name</span>
-          </v-btn>
-          <span>Sort by project name</span>
-        </v-tooltip>
-        <v-tooltip top>
-          <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
-            <v-icon small left>person</v-icon>
-            <span class="caption text-lowercase">By Person</span>
-          </v-btn>
-          <span>Sort by project author</span>
-        </v-tooltip>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="project in projects" :key="project.name">
+          <v-card flat class="text-xs-center ma-3">
+            <v-responsive class="pt-4">
+                 <v-img
+      class="white--text align-end"  height="200px"  :src="project.src">
+      <v-card-title>{{ project.name }}</v-card-title>
+        </v-img>
+        
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ project.name }}</div>
+              <div class="grey--text">{{ project.difficulty }}</div>
+            </v-card-text>
+   <v-card-actions>
+    <template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" scrollable max-width="300px" :retain-focus="false">
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark v-on="on" v-on:click="getAccountDetails()">Goto Account</v-btn>
+      </template>
+      <v-card>
+        <v-card-title>{{ project.name }}</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text style="height: 300px;">
+          
+        </v-card-text>
+        <v-divider></v-divider>
+        {{tmp}}
+        
+        <v-card-actions>
+          <v-btn color="blue darken-1" text @click="launchAccount()">Launch Account</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+            
+   </v-card-actions>
+
+  </v-card>
+          <!-- </v-card> -->
+          
+        </v-flex>
       </v-layout>
       
-      <v-card flat v-for="project in projects" :key="project.title">
-        <v-layout row wrap :class="`pa-3 project ${project.status}`">
-          <v-flex xs12 md6>
-            <div class="caption grey--text">Project title</div>
-            <div>{{ project.title }}</div>
-          </v-flex>
-          <v-flex xs6 sm4 md2>
-            <div class="caption grey--text">Person</div>
-            <div>{{ project.person }}</div>
-          </v-flex>
-          <v-flex xs6 sm4 md2>
-            <div class="caption grey--text">Due by</div>
-            <div>{{ project.due }}</div>
-          </v-flex>
-          <v-flex xs2 sm4 md2>
-            <div class="right">
-              <v-chip small :class="`${project.status} white--text my-2 caption`">{{ project.status }}</v-chip>
-            </div>
-          </v-flex>
-        </v-layout>
-        <v-divider></v-divider>
-      </v-card>
 
     </v-container>
-   
+    
   </div>
 </template>
 
 <script>
-import db from '@/fb'
-
+import axios from 'axios'
 export default {
-  data() {
-    return {
-      projects: []
-    }
-  },
-  methods: {
-    sortBy(prop) {
-      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
-    }
-  },
-  created() {
-    db.collection('projects').onSnapshot(res => {
-      const changes = res.docChanges();
 
-      changes.forEach(change => {
-        if (change.type === 'added') {
-          this.projects.push({
-            ...change.doc.data(),
-            id: change.doc.id
-          })
-        }  
-      })
-    })
+  account_description:{},
+  data() {
+  
+    return {
+      projects: [
+        { name: 'VPC, Subent,EC2 ', difficulty: 'Moderate', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg' },
+        { name: 'DynamoDb,API  Gateway, Lambda', difficulty: 'Moderate', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg' },
+        { name: 'S3 Cloudfront', difficulty: 'Critical', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg' },
+        { name: 'Code Commit,Code Deploy,Code Star', difficulty: 'Moderate', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg' },
+        { name: 'SQS and SNS', difficulty: 'Easy', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg'}
+
+      ],
+       dialog: false,
+        notifications: false,
+        sound: true,
+        widgets: false,
+        account_description:{},
+        account_used:[],
+      
+    }
+  },
+  methods:{
+    getAccountDetails(){
+      var account_description={}
+     console.log(account_description)
+       return new Promise( function(resolve) {
+       
+        axios.get('https://dx5biqoyme.execute-api.us-east-1.amazonaws.com/Stage/getAccount?accountname=ec2public')
+            .then( function(json) {
+            var tmp=JSON.parse(json.data.body)
+               
+               this.account_description=tmp
+
+                // The data from the request is available in a .then block
+                // We return the result using resolve.
+                resolve(json);
+            });
+    });
+   
+    },
+
+
+     launchAccount(){
+  
+    return new Promise( function(resolve) {
+        axios.get('https://dx5biqoyme.execute-api.us-east-1.amazonaws.com/Stage/getAccount?accountname=ec2public')
+            .then( function(json) {
+
+                // The data from the request is available in a .then block
+                // We return the result using resolve.
+                resolve(json);
+            });
+    });
+
+    }
+
+
+    
   }
 }
 </script>
-
-<style>
-
-.project.complete{
-  border-left: 4px solid #3cd1c2;
-}
-.project.ongoing{
-  border-left: 4px solid #ffaa2c;
-}
-.project.overdue{
-  border-left: 4px solid #f83e70;
-}
-.v-chip.complete{
-  background: #3cd1c2;
-}
-.v-chip.ongoing{
-  background: #ffaa2c;
-}
-.v-chip.overdue{
-  background: #f83e70;
-}
-
-</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,104 +1,136 @@
 <template>
-  <div class="dashboard">
-    <h1 class="subheading grey--text">Dashboard</h1>
+  <div class="team">
+    <h1 class="subheading grey--text">Choose an Account type you want to vend</h1>
 
     <v-container class="my-5">
 
-      <v-layout row justify-start class="mb-3">
-        <v-tooltip top>
-          <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
-            <v-icon small left>folder</v-icon>
-            <span class="caption text-lowercase">By project name</span>
-          </v-btn>
-          <span>Sort by project name</span>
-        </v-tooltip>
-        <v-tooltip top>
-          <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
-            <v-icon small left>person</v-icon>
-            <span class="caption text-lowercase">By Person</span>
-          </v-btn>
-          <span>Sort by project author</span>
-        </v-tooltip>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="project in projects" :key="project.name">
+          <v-card flat class="text-xs-center ma-3">
+            <v-responsive class="pt-4">
+                 <v-img
+      class="white--text align-end"  height="200px"  :src="project.src">
+      <v-card-title>{{ project.name }}</v-card-title>
+        </v-img>
+        
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ project.name }}</div>
+              <div class="grey--text">{{ project.difficulty }}</div>
+            </v-card-text>
+   <v-card-actions>
+    <template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" scrollable max-width="300px" :retain-focus="false">
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark v-on="on" v-on:click="getAccountDetails()">Goto Account</v-btn>
+      </template>
+      <v-card>
+        <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">{{account_description['accconttype']}}</div>
+        <v-list-item-title class="headline mb-1">{{account_description['accconttype']}}</v-list-item-title>
+        <v-list-item-subtitle>{{account_description['accountdescription']}}</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+       </v-list-item>
+       
+        
+        <v-card-actions>
+          <v-btn color="blue darken-1" text @click="launchAccount()">Launch Account</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+            
+   </v-card-actions>
+
+  </v-card>
+          <!-- </v-card> -->
+          
+        </v-flex>
       </v-layout>
       
-      <v-card flat v-for="project in projects" :key="project.title">
-        <v-layout row wrap :class="`pa-3 project ${project.status}`">
-          <v-flex xs12 md6>
-            <div class="caption grey--text">Project title</div>
-            <div>{{ project.title }}</div>
-          </v-flex>
-          <v-flex xs6 sm4 md2>
-            <div class="caption grey--text">Person</div>
-            <div>{{ project.person }}</div>
-          </v-flex>
-          <v-flex xs6 sm4 md2>
-            <div class="caption grey--text">Due by</div>
-            <div>{{ project.due }}</div>
-          </v-flex>
-          <v-flex xs2 sm4 md2>
-            <div class="right">
-              <v-chip small :class="`${project.status} white--text my-2 caption`">{{ project.status }}</v-chip>
-            </div>
-          </v-flex>
-        </v-layout>
-        <v-divider></v-divider>
-      </v-card>
 
     </v-container>
-   
+    
   </div>
 </template>
 
 <script>
-import db from '@/fb'
-
+import axios from 'axios'
 export default {
-  data() {
-    return {
-      projects: []
-    }
-  },
-  methods: {
-    sortBy(prop) {
-      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
-    }
-  },
-  created() {
-    db.collection('projects').onSnapshot(res => {
-      const changes = res.docChanges();
 
-      changes.forEach(change => {
-        if (change.type === 'added') {
-          this.projects.push({
-            ...change.doc.data(),
-            id: change.doc.id
-          })
-        }  
-      })
-    })
+  account_description:{},
+  data() {
+  
+    return {
+      projects: [
+        { name: 'VPC, Subent,EC2 ', difficulty: 'Moderate', src: '/avatar-10.png' },
+        { name: 'DynamoDb,API  Gateway, Lambda', difficulty: 'Moderate', src: '/avatar-9.png' },
+        { name: 'S3 Cloudfront', difficulty: 'Critical', src: '/avatar-8.png' },
+        { name: 'Code Commit,Code Deploy,Code Star', difficulty: 'Moderate', src: '/avatar-7.png' },
+        { name: 'SQS and SNS', difficulty: 'Easy', src: '/avatar-6.png'}
+
+      ],
+       dialog: false,
+        notifications: false,
+        sound: true,
+        widgets: false,
+        account_description:{},
+        account_used:[],
+      
+    }
+  },
+  methods:{
+    getAccountDetails(){
+      var account_description={}
+     console.log(account_description)
+     
+       
+        axios.get('https://dx5biqoyme.execute-api.us-east-1.amazonaws.com/Stage/getAccount?accountname=ec2public')
+            .then( function(json) {
+            var tmp=JSON.parse(json.data.body)
+             //console.log(tmp)  
+               this.account_description=tmp
+
+                // The data from the request is available in a .then block
+                // We return the result using resolve.
+              
+            
+    }.bind(this));
+   
+    },
+
+
+     launchAccount(){
+  
+    return new Promise( function(resolve) {
+        axios.get('https://40gi94x7sk.execute-api.us-east-1.amazonaws.com/Stage/sts?uName=subrata')
+            .then( function(json) {
+                 console.log(json)
+                  console.log(json['data']['body']['url'])
+                  var url=json['data']['body']['url']
+                 // window.location.href = url
+                  var strWindowFeatures = "location=yes,height=570,width=520,scrollbars=yes,status=yes";
+                  window.open(url, "_blank", strWindowFeatures);
+                // The data from the request is available in a .then block
+                // We return the result using resolve.
+                resolve(json);
+            });
+    });
+
+    }
+
+
+    
   }
 }
 </script>
-
-<style>
-
-.project.complete{
-  border-left: 4px solid #3cd1c2;
-}
-.project.ongoing{
-  border-left: 4px solid #ffaa2c;
-}
-.project.overdue{
-  border-left: 4px solid #f83e70;
-}
-.v-chip.complete{
-  background: #3cd1c2;
-}
-.v-chip.ongoing{
-  background: #ffaa2c;
-}
-.v-chip.overdue{
-  background: #f83e70;
-}
-
-</style>
